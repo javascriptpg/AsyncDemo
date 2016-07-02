32d69a404e5dcde7155931b585c79d474979a594
JerryChuang <jerrychuang@nine-yi.com>
2016-07-02 14:45:03 +0800

    建立UI

--- AsyncDemo/Demo.Designer.cs
+++ AsyncDemo/Demo.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnSync = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtInput.Location = new System.Drawing.Point(13, 13);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(443, 180);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 208);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(443, 180);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(462, 13);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(126, 23);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
             // 
             // Demo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
             this.Name = "Demo";
             this.Text = "DemoForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnSync;
     }
 }
 
